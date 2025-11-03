3753fd8d5cc1b65aac2e4baf090dca0a36931873
Gopal <gopalmehtre10@gmail.com>
2025-11-04 01:43:41 +0530

    Support section

--- frontend/src/landing_page/support/SupportPage.jsx
+++ frontend/src/landing_page/support/SupportPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CreateTicket from './CreateTicket'
+import Hero from './Hero'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
 
 export default function SupportPage() {
   return (
     <>
-    <h1>SupportPage</h1></>
+    <Hero />
+    <CreateTicket />
+    </>
   )
 }
