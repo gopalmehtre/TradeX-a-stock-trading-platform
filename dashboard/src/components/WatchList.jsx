637c753a25f462f40017fa58b72fe0c717f3dc49
Gopal <gopalmehtre10@gmail.com>
2025-11-05 02:29:08 +0530

    creating-dashboard

---
+++ dashboard/src/components/WatchList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function WatchList() {
+  return (
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts"> 9 / 50</span>
+      </div>
+
+      <ul className="list"></ul>
+    </div>
+  )
+}
